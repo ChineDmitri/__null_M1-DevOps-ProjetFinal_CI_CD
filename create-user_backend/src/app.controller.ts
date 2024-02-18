044465cc20a130ccbe6cc81e0954019a3de66c6c
ChineDmitri <mr.dima11@gmail.com>
2024-02-18 18:36:52 +0100

    correction

--- create-user_backend/src/app.controller.ts
+++ create-user_backend/src/app.controller.ts
@@ -7,8 +7,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    console.log(process.env.MYSQL_DB_PORT_PROD);
-    console.log('x');
     return this.appService.getHello();
   }
 }
